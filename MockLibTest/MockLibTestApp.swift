4accf96e7cb59e3ffe25c27290483e648f791df6
Juan S Orozco Buitrago <sebasorozcob@gmail.com>
2023-12-19 09:59:00 -0500

    Initial Commit

---
+++ MockLibTest/MockLibTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockLibTestApp.swift
+//  MockLibTest
+//
+//  Created by Juan Sebastian Orozco Buitrago on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MockLibTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
